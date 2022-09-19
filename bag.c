5c7248afa97903232e913bffb85e794874a1ea9c
vitu <86437342+kbanaVitu@users.noreply.github.com>
2022-09-19 20:14:47 -0300

    Update bag.c

--- bag.c
+++ bag.c
@@ -37,17 +37,36 @@ int insert(Bag *bag, int e)
 
 int get(Bag *bag)
 {
+    int i = 0;
+    do
+    {
+        i = rand() % bag->n; // atribui um valor a i, tal que 0 <= i < n
+    } while (bag->data[i] == 0); // avalia se em i ainda não tem um elemento
+    bag->data[i] = 0;
+    bag->size--;
     return 0;
 }
 
 int search(Bag *bag, int e)
 {
-    return 0;
+    int aux = 0;
+    for(int i = 0;i<=bag->n;i++)
+        if(e == bag->data[i]) // avalia se 'e' corresponde ao elemento no vetor
+        aux++;
+    if (aux >= 1)
+        return printf("O valor %i está na bag\n\n", e);
+    else
+        return printf("O valor %i não está na bag\n\n", e);
 }
 
 int size(Bag *bag)
 {
-    return 0;
+    return printf("Número de elementos contido na bag: %i\n\n", bag->size);
 }
 
-void printAll(Bag *bag) {}
+void printAll(Bag *bag) {
+    printf("Valores contidos na bag:");
+    for (int i = 0; i < bag->n; i++)
+        printf("%i ", bag->data[i]);
+        printf("\n\n");
+}
